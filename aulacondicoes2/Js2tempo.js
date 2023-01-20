01fe72e290a6c78f5bd9d5597e59f9d4cc93208d
Humberto <humbertosilveira52@gmail.com>
2023-01-20 20:28:25 -0300

    JS

---
+++ aulacondicoes2/Js2tempo.js
@@ -0,0 +1,11 @@
+hora = 3
+
+if (hora <= 12 && hora >= 6) {
+    console.log('Bom Dia!')
+}else if (hora <= 18 && hora >= 13) {
+    console.log('Boa Tarde!')
+}else if (hora <= 23 && hora >= 19) {
+    console.log('Boa Noite!')
+}if (hora <= 5 && hora >= 1) {
+    console.log('Boa Madruga')
+}
---
+++ aulacondicoes2/Jsvotos.js
@@ -0,0 +1,10 @@
+idade = 16
+if (idade < 16) {
+    console.log('Não Vota')
+}else{
+    if (idade >= 16 && idade < 18) {
+        console.log('Vota Opcionalmente')
+    }else {
+        console.log('Vota')
+    }
+}
