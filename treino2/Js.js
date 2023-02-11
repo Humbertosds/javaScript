da0e4ddb84d7d39f75b43fde8ba19d805e1a2dfd
Humberto <humbertosilveira52@gmail.com>
2023-02-11 15:44:51 -0300

    .

---
+++ treino2/Js.js
@@ -0,0 +1,8 @@
+let nome = document.getElementById('inome')
+let altura = document.getElementById('ialtura')
+let peso = document.getElementById('ipeso')
+let resu = document.getElementById('res')
+
+function Calcular() {
+    
+}
