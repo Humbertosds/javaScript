4b461f612a524083ea3cdc7670ac4cff61031aa9
Humberto <humbertosilveira52@gmail.com>
2023-02-13 16:45:18 -0300

    .

--- treino2/Js.js
+++ treino2/Js.js
@@ -1,13 +1,25 @@
-
-
 function Calcular() {
     let nome = document.getElementById('inome').value;
     let altura = document.getElementById('ialtura').value;
     let peso = document.getElementById('ipeso').value;
     let resu = document.getElementById('res')
-
     if(nome !== '' && altura !== '' && peso !== '') {
-        
+        const valorIMC = (peso / (altura * altura)).toFixed(1)
+        let classificacao = ''
+        if (valorIMC < 18.5) {
+            classificacao = 'Abaixo do Peso'
+        }else if (valorIMC < 25) {
+            classificacao = 'Peso ideal'
+        }else if (valorIMC < 30) {
+            classificacao = 'Levemente acima do peso'
+        }else if (valorIMC < 35) {
+            classificacao = 'Acima do peso'
+        }else if (valorIMC < 40) {
+            classificacao = 'Bem acima do peso'
+        }else {
+            classificacao = 'OBESO(A)'
+        }
+        resu.textContent = `${nome} seu IMC é ${valorIMC} e voçê esta ${classificacao}`;
     }else {
         resu.textContent = 'Preencha os campos acima'
         
