d9de7de13fc5ca1e304c1297aa6a5a967a46e2ac
Humberto <humbertosilveira52@gmail.com>
2023-02-14 16:22:59 -0300

    lampada

--- treino3/lampada.js
+++ treino3/lampada.js
@@ -1,6 +1,7 @@
 let turnOn = document.getElementById('turnOn')
 let turnOff = document.getElementById('turnOff')
 let lamp = document.getElementById('lamp')
+let turnOnOff = document.getElementById('turnOnOff')
 
 function lampOn() {
     lamp.src = './imgs/ligada.jpg'
@@ -14,7 +15,16 @@ function lampbroken() {
     lamp.src = './imgs/quebrada.jpg'
 }
 
+function lampOnOff() {
+    if (turnOnOff.textContent == 'Ligar') {
+        lampOn()
+        turnOnOff.textContent = 'Desligar'
+    }else {
+        lampOff()
+        turnOnOff.textContent = 'Ligar'
+    }
+}
 
-turnOn.addEventListener('click', lampOn)
-turnOff.addEventListener('click', lampOff)
+
+turnOnOff.addEventListener('click', lampOnOff)
 lamp.addEventListener('dblclick', lampbroken)
