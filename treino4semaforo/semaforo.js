1fec4bf5c503ad5724eeb5f554603ab4f61e928e
Humberto <humbertosilveira52@gmail.com>
2023-02-15 08:13:32 -0300

    .

--- treino4semaforo/semaforo.js
+++ treino4semaforo/semaforo.js
@@ -1,4 +1,13 @@
+const img = document.getElementById('img')
+const buttons = document.getElementById('buttons')
 
+const trafficlgh = () => {
+
+}
+
+
+
+buttons.addEventListener('click', trafficlght)
 
 
 
