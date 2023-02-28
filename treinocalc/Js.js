e531e4a0f270fc023f89eeefe5a709ebb147df97
Humberto <humbertosilveira52@gmail.com>
2023-02-28 19:22:52 -0300

    .

--- treinocalc/Js.js
+++ treinocalc/Js.js
@@ -1,1 +1,23 @@
 
+
+function insert(n) {
+    document.getElementById('current-calc').innerHTML += n;
+
+}
+
+function clean() {
+    document.getElementById('current-calc').innerHTML = "";
+}
+
+function tirar1() {
+    resultado = document.getElementById('current-calc').innerHTML;
+    document.getElementById('current-calc').innerHTML = resultado.substring(0, resultado.length -1);
+}
+
+function calcular() {
+    resultado = document.getElementById('current-calc').innerHTML;
+    if(resultado) {
+        document.getElementById('current-calc').innerHTML = eval(resultado)
+    }
+
+}
